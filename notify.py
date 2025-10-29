c1523195de41c38aa3efdd219e50b360d8327763
Platon Rodionov <plotoh@inbox.ru>
2025-10-29 17:00:59 +0500

    Add priority to notification

--- notify.py
+++ notify.py
@@ -6,30 +6,34 @@ import datetime as dt
 
 class Notification:
     DATE_FORMATS = [
-        "%Y-%m-%d",          # 2025-10-28
-        "%d/%m/%Y",          # 28/10/2025
-        "%d.%m.%Y",          # 28.10.2025
-        "%d-%m-%Y",          # 28-10-2025
-        "%Y/%m/%d",          # 2025/10/28
-        "%B %d, %Y",         # October 28, 2025
-        "%b %d, %Y",         # Oct 28, 2025
-        "%d %B %Y",          # 28 October 2025
-        "%d %b %Y",          # 28 Oct 2025
+        "%Y-%m-%d",  # 2025-10-28
+        "%d/%m/%Y",  # 28/10/2025
+        "%d.%m.%Y",  # 28.10.2025
+        "%d-%m-%Y",  # 28-10-2025
+        "%Y/%m/%d",  # 2025/10/28
+        "%B %d, %Y",  # October 28, 2025
+        "%b %d, %Y",  # Oct 28, 2025
+        "%d %B %Y",  # 28 October 2025
+        "%d %b %Y",  # 28 Oct 2025
     ]
 
-    def __init__(self, user_id: str, notification: tuple[str, str|dt.date] = None):
+    def __init__(self, user_id: str, notification: tuple[str, str | dt.date, str | None] = None):
         self.user_id = user_id
-        self.message, self.send_at = None, None
+        self.message, self.send_at, self.priority = None, None, None
         try:
             self.get_notification(notif=notification)
         except ValueError as ve:
             print(f'Не удалось создать уведомление. Ошибка: {ve}')
 
     def get_notification(self, notif=None):
-        if not (isinstance(notif, tuple) and len(notif) == 2):
+        if not isinstance(notif, tuple) and len(notif) in [2, 3]:
             raise ValueError(f"Неверное уведомление {notif}")
 
-        msg, send_at = notif
+        if len(notif) == 2:
+            msg, send_at = notif
+            priority = 'normal'
+        else:
+            msg, send_at, priority = notif
 
         if not isinstance(msg, str):
             raise ValueError(f"Уведомление {msg} не является строкой")
@@ -43,17 +47,7 @@ class Notification:
         else:
             raise ValueError(f"Неверный формат даты: {send_at}")
 
-        self.message, self.send_at = msg, parsed_date
-
-    # def send_notification(self):
-    #     today = dt.date.today()
-    #     if self.send_at and self.send_at <= today:
-    #         message = self.message
-    #         if message:
-    #             print(f'INFO: Sending message: {message} with date {self.send_at} to user with id: {self.user_id}')
-    #             # return f'INFO: Sending message: {message} with date {self.date} to user with id: {self.user_id}'
-    #     else:
-    #         print("уведомления нет. не знаю почему. но мы разберемся. наверное")
+        self.message, self.send_at, self.priority = msg, parsed_date, priority
 
     def parse_date_string(self, date_str: str) -> dt.date | None:
         """
@@ -80,8 +74,9 @@ class Schedule:
                 'id': self.UID,
                 'user_id': notif.user_id,
                 'message': notif.message,
-                'send_at': notif.send_at
-                         }
+                'send_at': notif.send_at,
+                'priority': notif.priority
+            }
             self.notifications.append(notif_dct)
             Schedule.UID += 1
 
@@ -93,14 +88,15 @@ class Schedule:
             if send_at and send_at <= today:
                 message = notif['message']
                 if message:
-                    print(f'INFO: Sending message: "{message}" with datetime {send_at} to user with id: {notif['user_id']}')
+                    print(
+                        f'INFO: Sending message: "{message}" with priority: {notif['priority']}, with datetime {send_at} to user with id: {notif['user_id']}')
                 else:
                     print("уведомления нет. не знаю почему. но мы разберемся. наверное")
             else:
                 continue
 
 
-notif1 = Notification("123", ("вот нахуя я так усложнил", "2025-10-28"))
+notif1 = Notification("123", ("вот нахуя я так усложнил", "2025-10-28", 'normal'))
 # print(notif.date)  # 2025-10-28
 notif2 = Notification("124", ("вот нахуя я так усложнил", "2025-11-28"))
 # print(notif.date)  # 2025-10-28
